cec4a1d3096b60836b73e257584208b8bb41d564
kinoshita_h <kinoshita_h@T1994019B>
2015-09-17 14:39:21 +0900

    新規作成

--- test/polymorphismSample/after/EmployeeTest.java
+++ test/polymorphismSample/after/EmployeeTest.java
@@ -48,64 +48,73 @@ public class EmployeeTest {
     
      @Test
      public void getSalary役員() {
+         String type = "役員";
          EmployeeFactory employeeFactory = new EmployeeFactory();
-         Employee employee = employeeFactory.getEmployeeInstanse("役員");
+         Employee employee = employeeFactory.getEmployeeInstanse(type);
          assertEquals(employee.getSalary(), 10000000);
      }
      
      @Test
      public void getSalary管理職() {
+         String type = "管理職";
          EmployeeFactory employeeFactory = new EmployeeFactory();
-         Employee employee = employeeFactory.getEmployeeInstanse("管理職");
+         Employee employee = employeeFactory.getEmployeeInstanse(type);
          assertEquals(employee.getSalary(), 4000000);
      }
      
      @Test
      public void getSalary一般職() {
+         String type = "一般職";
          EmployeeFactory employeeFactory = new EmployeeFactory();
-         Employee employee = employeeFactory.getEmployeeInstanse("一般職");
+         Employee employee = employeeFactory.getEmployeeInstanse(type);
          assertEquals(employee.getSalary(), 2500000);
      }
      
      @Test
      public void get役職手当役員() {
+         String type = "役員";
          EmployeeFactory employeeFactory = new EmployeeFactory();
-         Employee employee = employeeFactory.getEmployeeInstanse("役員");
+         Employee employee = employeeFactory.getEmployeeInstanse(type);
          assertEquals(employee.getPositionAllowance(2000000), 200000);
      }
      
      @Test
      public void get役職手当管理職() {
+         String type = "管理職";
          EmployeeFactory employeeFactory = new EmployeeFactory();
-         Employee employee = employeeFactory.getEmployeeInstanse("管理職");
+         Employee employee = employeeFactory.getEmployeeInstanse(type);
          assertEquals(employee.getPositionAllowance(111111), 5555);
      }
      
      @Test
      public void get役職手当一般職() {
+         String type = "一般職";
          EmployeeFactory employeeFactory = new EmployeeFactory();
-         Employee employee = employeeFactory.getEmployeeInstanse("一般職");
+         Employee employee = employeeFactory.getEmployeeInstanse(type);
          assertEquals(employee.getPositionAllowance(3333333), 0);
      }
      
      @Test
      public void get時間外手当役員() {
+         String type = "役員";
          EmployeeFactory employeeFactory = new EmployeeFactory();
-         Employee employee = employeeFactory.getEmployeeInstanse("役員");
+         Employee employee = employeeFactory.getEmployeeInstanse(type);
          assertEquals(employee.getOvertimeAllowance(30, 300000), 0);
      }
      
      @Test
      public void get時間外手当管理職() {
+         String type = "管理職";
          EmployeeFactory employeeFactory = new EmployeeFactory();
-         Employee employee = employeeFactory.getEmployeeInstanse("管理職");
+         Employee employee = employeeFactory.getEmployeeInstanse(type);
          assertEquals(employee.getOvertimeAllowance(30, 300000), 9375);
      }
      
      @Test
      public void get時間外手当一般職() {
+         String type = "一般職";
          EmployeeFactory employeeFactory = new EmployeeFactory();
-         Employee employee = employeeFactory.getEmployeeInstanse("一般職");
+         Employee employee = employeeFactory.getEmployeeInstanse(type);
          assertEquals(employee.getOvertimeAllowance(30, 300000), 14062);
      }
 }
