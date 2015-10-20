94566248789d9fe4542e112f2c78c54dfe026851
木下英樹 <kinopp@myhomene.jp>
2015-10-20 13:03:16 +0900

    リファクタリング

--- src/polymorphismSample/before/Payroll.java
+++ src/polymorphismSample/before/Payroll.java
@@ -5,8 +5,6 @@ package polymorphismSample.before;
  * @author kinoshita_h
  */
 public class Payroll {
-    //int salary = 0;
-    
     /**
      * 給料を取得する
      * @param type 役職
@@ -27,15 +25,15 @@ public class Payroll {
     /**
      * 役職手当を取得する
      * @param type 役職
-     * @param baseAmount 基準額 
+     * @param salary 給料
      * @return 役職手当
      */
-    public int getPositionAllowance(String type, int baseAmount){
+    public int getPositionAllowance(String type, int salary){
         int positionAllowance = 0;
         if (type.equals("役員")){
-            positionAllowance = (int)(baseAmount * 0.1);
+            positionAllowance = (int)(salary * 0.1);
         } else if (type.equals("管理職")){
-            positionAllowance = (int)(baseAmount * 0.05);
+            positionAllowance = (int)(salary * 0.05);
         }
         return positionAllowance;
     }
@@ -44,15 +42,15 @@ public class Payroll {
      * 時間外手当を取得する
      * @param type 役職
      * @param overtime 残業時間
-     * @param baseAmount 基準額 
+     * @param salary 給料
      * @return 時間外手当
      */
-    public int getOvertimeAllowance(String type, int overtime, int baseAmount){
+    public int getOvertimeAllowance(String type, int overtime, int salary){
         int overtimeAllowance = 0;
         if (type.equals("管理職")){
-            overtimeAllowance = (int)(baseAmount/240 * 0.25 * overtime);
+            overtimeAllowance = (int)(salary /240 * 0.25 * overtime);
         } else if (type.equals("一般職")){
-            overtimeAllowance = (int)(baseAmount/160 * 0.25 * overtime);
+            overtimeAllowance = (int)(salary /160 * 0.25 * overtime);
         }
         return overtimeAllowance;
     }
