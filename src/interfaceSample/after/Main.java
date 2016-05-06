9fd39c06431964d2318e26d369b381295db23e89
kinopp <kinopp@192.168.11.2>
2016-05-06 20:43:46 +0900

    シート追加

--- src/interfaceSample/after/Main.java
+++ src/interfaceSample/after/Main.java
@@ -8,11 +8,11 @@ public class Main {
         //書類作成者オブジェクトを生成
         DocumentMaker documentMaker = new DocumentMaker();
         //書けるものとしてボールペンオブジェクトを生成
-        Writable ballpointPen = new BallpointPen();
+        Writable ballpointPen       = new BallpointPen();
         //書けるものとして万年筆オブジェクトを生成
-        Writable fountainPen = new FountainPen();
+        Writable fountainPen        = new FountainPen();
         //書けるものとしてシャープペンシルオブジェクトを生成
-        Writable mechanicalPencil = new MechanicalPencil();
+        Writable mechanicalPencil   = new MechanicalPencil();
         
         //ボールペンで書類を作成
         documentMaker.documentMake(ballpointPen);
--- src/interfaceSample/before/Main.java
+++ src/interfaceSample/before/Main.java
@@ -6,11 +6,11 @@ package interfaceSample.before;
 public class Main {
     public static void main(String... args){
         //書類作成者オブジェクトを生成
-        DocumentMaker documentMaker = new DocumentMaker();
+        DocumentMaker documentMaker       = new DocumentMaker();
         //ボールペンとしてボールペンオブジェクトを生成
-        BallpointPen ballpointPen = new BallpointPen();
+        BallpointPen ballpointPen         = new BallpointPen();
         //万年筆として万年筆オブジェクトを生成
-        FountainPen fountainPen = new FountainPen();
+        FountainPen fountainPen           = new FountainPen();
         //シャープペンシルとしてシャープペンシルオブジェクトを生成
         MechanicalPencil mechanicalPencil = new MechanicalPencil();
         
